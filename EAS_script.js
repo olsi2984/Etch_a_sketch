2653b1f15794818e0b1e13b354380fcc2418c0f2
Olsi Qatipi <olsi2984@yahoo.com>
2019-10-29 23:42:58 +0100

    Completed grid and added hover efect

--- EAS_script.js
+++ EAS_script.js
@@ -1,13 +1,27 @@
-function createGrid(n) {    
+function createGrid(n) {      
      
     for (let index = 0; index < n*n; index++) {
-        
+                
         const container = document.querySelector('#container');
-        const div1 = document.createElement("div");
-        container.appendChild (div1);
+        const div1 = document.createElement("div");        
         
+        container.appendChild (div1);
+
+        container.style.setProperty('--grid-dimension', n);
+        div1.setAttribute("id", "gridcells");    
+    
     }
 
 }
 
 createGrid(16);
+
+
+var cells = Array.from(document.querySelectorAll('#gridcells'));
+cells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "black";
+    });
+});
+
+
