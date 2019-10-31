96ef08eac67496eec0c69810df150a9e5e7652df
Olsi Qatipi <olsi2984@yahoo.com>
2019-11-01 00:05:30 +0100

    Added clears scetchpad button

--- EAS_script.js
+++ EAS_script.js
@@ -1,21 +1,19 @@
+(function () {
+    createGrid(16);
+})();
+
 function createGrid(n) {      
      
     for (let index = 0; index < n*n; index++) {
                 
         const container = document.querySelector('#container');
-        const div1 = document.createElement("div");        
-        
+        const div1 = document.createElement("div");      
         container.appendChild (div1);
-
         container.style.setProperty('--grid-dimension', n);
-        div1.setAttribute("id", "gridcells");    
-    
+        div1.setAttribute("id", "gridcells");       
     }
 
 }
-
-createGrid(16);
-
 
 var cells = Array.from(document.querySelectorAll('#gridcells'));
 cells.forEach((cell) => {
@@ -24,4 +22,20 @@ cells.forEach((cell) => {
     });
 });
 
+var btn1 = document.querySelector('#btn1');
+btn1.addEventListener("click", function(){
+
+    var cells = Array.from(document.querySelectorAll('#gridcells'));
+    cells.forEach((cell) => {    
+        cell.style.backgroundColor = "transparent";
+    });
+
+    
+});
+          
+function changeGrid() {
+    let n = prompt ("Please type the number of squares per side you would like in your scetchpad");      
+    createGrid(n);
+
+} 
 
